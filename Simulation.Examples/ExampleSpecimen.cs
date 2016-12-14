12569cd6411aedbde0b8ea56b2f2cabee35b5a98
Koen Groener <koengroener@outlook.com>
2016-12-14 23:17:03 +0100

    Added example factory and specimen

---
+++ Simulation.Examples/ExampleSpecimen.cs
@@ -0,0 +1,71 @@
+﻿using Simulation.Interface.Specimen;
+using Simulation.Interface.Specimen.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Windows.Media;
+
+namespace Simulation.Examples
+{
+    internal class ExampleSpecimen : ISpecimen
+    {
+        private readonly EyeAttribute _eyeAttribute;
+        private readonly MotorAttribute _motorAttribute;
+        private Random _random;
+
+        public ExampleSpecimen()
+        {
+            _eyeAttribute = new EyeAttribute(0);
+            _motorAttribute = new MotorAttribute();
+            _random = new Random();
+
+        }
+
+        public IEnumerable<SpecimenAttributeBase> Attributes
+        {
+            get
+            {
+                return new SpecimenAttributeBase[]
+                {
+                    _eyeAttribute,
+                    _motorAttribute
+                };
+            }
+        }
+
+        public Color Color => Colors.Red;
+
+        public IEnumerable<Vector2> PolygonPoints
+        {
+            get
+            {
+                return new Vector2[]
+                {
+                    new Vector2(0,0),
+                    new Vector2(0,5),
+                    new Vector2(5,5),
+                    new Vector2(5,0),
+                };
+            }
+        }
+
+        public void Update()
+        {
+            if (_eyeAttribute.DetectionDistance.HasValue
+                && _eyeAttribute.DetectionDistance.Value < 100
+                )
+            {
+                if (_motorAttribute.Steering != 0)
+                {
+                    _motorAttribute.Steering = _random.Next(0, 2) == 0 ? -1 : 1;
+                    _motorAttribute.Throttle = 0.2;
+                }
+            }
+            else
+            {
+                _motorAttribute.Throttle = 1;
+                _motorAttribute.Steering = 0;
+            }
+        }
+    }
+}
