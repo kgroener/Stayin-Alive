0826f70c7ea409930420153dc34331d547226b77
Tom Kuijsten <devkoes@outlook.com>
2016-12-24 14:57:44 +0100

    Added export attribute

--- Simulation.Examples/ExampleSimulationMetadata.cs
+++ Simulation.Examples/ExampleSimulationMetadata.cs
@@ -1,9 +1,11 @@
 ﻿using Simulation.Interface;
 using Simulation.Interface.Logging;
 using Simulation.Interface.Specimen;
+using System.Composition;
 
 namespace Simulation.Examples
 {
+    [Export(typeof(ISimulationMetadata))]
     public class ExampleSimulationMetadata : ISimulationMetadata
     {
         public string SpecieName => "ExampleSpecie";
